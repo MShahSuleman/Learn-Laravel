f18fa899cd47c8f42aaa283271411a9ba5e26d79
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-11-01 20:21:13 +0500

    Update songs.blade.php

--- e-Project/resources/views/pages/songs.blade.php
+++ e-Project/resources/views/pages/songs.blade.php
@@ -276,18 +276,18 @@
  
 <div class="section" >
     <h2>To get you started</h2>
-    {{-- <a href="{{ url('/open-letter') }}"> --}}
+    <a href="{{ url('/open-letter') }}">
         <div class="card"  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    <svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 24 24" class="Svg-sc-ytk21e-0 bneLcE play" width="24" height="24">
-                        <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z" fill="black"></path>
-                    </svg>
-                    <img alt="Album cover" height="200" src="./src/open letter.jpg" width="200"/>
-                    <div class="title">Open Letter</div>
-                    <div class="description card-title">
+          <svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 24 24" class="Svg-sc-ytk21e-0 bneLcE play" width="24" height="24">
+              <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z" fill="black"></path>
+          </svg>
+            <img alt="Album cover" height="200" src="./src/open letter.jpg" width="200"/>
+            <div class="title">Open Letter</div>
+            <div class="description card-title">
                         Faisal Kapadia, aleemrk and Talhah Anjum
-                    </div>
+          </div>
         </div>
-{{-- </a> --}}
+</a>
 
     <div class="card">   
        <!-- 2nd Card Start -->
