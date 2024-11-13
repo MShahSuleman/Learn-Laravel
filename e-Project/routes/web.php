572019fc28513dde852e73c04853c8249a9273d9
M Suleman <163521322+MSulemankashif@users.noreply.github.com>
2024-11-13 20:36:24 +0500

    back-end

--- e-Project/routes/web.php
+++ e-Project/routes/web.php
@@ -6,41 +6,26 @@ use App\Models\Song;
 use App\Http\Controllers\personController;
 
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
-// Route::get('/open-letter', function () {
-//     return view('pages.open-letter');
-// });
+Route::get('/open-letter', function () {
+    return view('pages.open-letter');
+});
 
-// Route::get('/login',function(){
-//     return view('pages.login');
-// });
-// Route::get('/about', function () {
-//     return view('pages.aboutus');
-// });
+Route::get('/login',function(){
+    return view('pages.login');
+});
+Route::get('/about', function () {
+    return view('pages.aboutus');
+});
 
-// // routes/web.php
-// Route::get('/song/{id}', [SongController::class, 'show'])->name('song.show');
+// routes/web.php
+Route::get('/song/{id}', [SongController::class, 'show'])->name('song.show');
 
-// Route::get('/', function () {
-//     $songs = Song::all(); // Fetch all songs from database
-//     return view('welcome', compact('songs'));
-// });
-
-
-
-Route::get('/indexPage', [personController::class, 'showPerson'])->name('home');
-
-Route::post('/save', [personController::class, 'savePerson'])->name('save');
-
-Route::view('/addUser', 'person.form')->name('add');
-// Route::view('/edit/{id}', 'person.editUser')->name('edit');
-
-Route::get('/edit/{id}', [personController::class, 'editPerson'])->name('edit');
-
-Route::get('/update', [personController::class, 'updatePerson'])->name('update');
-
-Route::get('delete/{id}', [personController::class, 'deletePerson'])->name('delete');
+Route::get('/', function () {
+    $songs = Song::all(); // Fetch all songs from database
+    return view('welcome', compact('songs'));
+});
